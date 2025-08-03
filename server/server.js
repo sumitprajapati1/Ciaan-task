9549947520d70da4df9bc15b45bf79850222c69d
sumit <prajapatisumit009@gmail.com>
2025-08-03 17:07:11 +0530

    solving cors error

--- server/server.js
+++ server/server.js
@@ -14,8 +14,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://ciaan-task.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://ciaan-task.vercel.app'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
