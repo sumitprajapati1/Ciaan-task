562cfdbb6d7973ce4d98dce7e42ba944d497b696
sumit <prajapatisumit009@gmail.com>
2025-08-03 17:03:24 +0530

    solving cors error

--- server/server.js
+++ server/server.js
@@ -13,7 +13,10 @@ dotenv.config({path: '.env'});
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://ciaan-task.vercel.app/'],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
